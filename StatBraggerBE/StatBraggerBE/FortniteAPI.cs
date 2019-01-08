2ca69a7621f798e7aec5e1a5428c4788093815cb
Anthony Cittadino <mycoolkook@gmail.com>
2019-01-08 13:57:09 -0500

    Added Fortnite API object

---
+++ StatBraggerBE/StatBraggerBE/FortniteAPI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace StatBraggerBE
+{
+    public class FortniteAPI
+    {
+        private string username;
+        private string platform;
+        private string content;
+
+        public FortniteAPI(string username, string platform)
+        {
+            this.username = username;
+            this.platform = platform;
+        }
+
+        public async void pullData()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.TryAddWithoutValidation("TRN-Api-Key", "8a6f583a-5d48-47f3-baa2-3ca659ed8eb5"); //Fortnite API Authorization and key
+                using (HttpResponseMessage response = await client.GetAsync("https://api.fortnitetracker.com/v1/profile/"+platform+"/"+username+""))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        this.content = await content.ReadAsStringAsync();
+                        Console.WriteLine(this.content);
+                    }
+                }
+            }
+        }
+        override
+        public string ToString()
+        {
+            return this.content;
+        }
+    }
+}
--- StatBraggerBE/StatBraggerBE/Program.cs
+++ StatBraggerBE/StatBraggerBE/Program.cs
@@ -11,24 +11,15 @@ namespace StatBraggerBE
     {
         static void Main(string[] args)
         {
-            string url = "https://api.fortnitetracker.com/v1/profile/pc/Tenor._.";
-            GetRequest(url); 
+            Console.Write("Enter fortnite username: ");
+            string name = Console.ReadLine();
+            Console.Write("\nEnter platform (psn, xbl, pc): ");
+            string platform = Console.ReadLine();
+
+            FortniteAPI player = new FortniteAPI(name,platform);
+            player.pullData();
+
             Console.ReadLine();
-        }
-        async static void GetRequest(string url)
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.TryAddWithoutValidation("TRN-Api-Key", "8a6f583a-5d48-47f3-baa2-3ca659ed8eb5");
-                using (HttpResponseMessage response = await client.GetAsync(url))
-                {
-                    using (HttpContent content = response.Content)
-                    {
-                        string mycontent = await content.ReadAsStringAsync();
-                        Console.WriteLine(mycontent);
-                    }
-                }   
-            }
         }
     }
 }
