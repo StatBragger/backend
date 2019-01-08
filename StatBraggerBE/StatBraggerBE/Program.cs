437cc5836e9f89e98760597aadb6e10a60adde7d
Anthony Cittadino <mycoolkook@gmail.com>
2019-01-08 12:48:07 -0500

    Added Fortnite Tracker API

--- StatBraggerBE/StatBraggerBE/Program.cs
+++ StatBraggerBE/StatBraggerBE/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace StatBraggerBE
 {
@@ -10,6 +11,24 @@ namespace StatBraggerBE
     {
         static void Main(string[] args)
         {
+            string url = "https://api.fortnitetracker.com/v1/profile/pc/Tenor._.";
+            GetRequest(url); 
+            Console.ReadLine();
+        }
+        async static void GetRequest(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.TryAddWithoutValidation("TRN-Api-Key", "8a6f583a-5d48-47f3-baa2-3ca659ed8eb5");
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string mycontent = await content.ReadAsStringAsync();
+                        Console.WriteLine(mycontent);
+                    }
+                }   
+            }
         }
     }
 }
